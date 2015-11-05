7c578aa164b59e5d115669a51d243d57079ca0aa
ReedKrawiec <reedkrawiec@gmail.com>
2015-11-05 09:09:52 -0500

    Site Interactions

--- scripts/alpha.navbar.js
+++ scripts/alpha.navbar.js
@@ -3,17 +3,13 @@ $(document).ready(function(){
   $(window).scroll(function(){
     var transitioning = 0;
     if($(window).scrollTop()>=oldtop){
-
       $("#Nav_Bar_Follows").removeClass("static");
       $("#Nav_Bar_Follows").addClass("follow")
-
       }
     else{
       console.log("readd")
       $("#Nav_Bar_Follows").removeClass("follow")
       $("#Nav_Bar_Follows").addClass("static")
-
-
     }
   })
   $('a').click(function(){
@@ -22,4 +18,19 @@ $(document).ready(function(){
       }, 500);
       return false;
   });
+
+  $(".tech_logo").hover(function(){
+    
+    console.log($(this).attr("data"));
+    var top = $(this).offset().top + $(this).height()+20;
+    /*";left:"+$(this).offset().left+*/
+    $("html").append("<div class='tech_box' style='display:none;top:"+top+"'><p>"+$(this).attr("data")+"</p></div>");
+    var left=($(this).width()-$(".tech_box").outerWidth())/2+$(this).position().left
+    console.log($(this).outerWidth()-$(".tech_box").outerWidth())
+    $(".tech_box").css("left",left).fadeIn(100);
+
+  },function(){
+    console.log("remove")
+    $(".tech_box").remove();
+  })
 })
