bbd7df5fd05b68ea9c66c19b73b47cf0dca391fb
Reed Krawiec <reedkrawiec@gmail.com>
2016-02-25 16:05:17 -0500

    updated to not use JQuery

--- babel/AppearOnScroll.js
+++ babel/AppearOnScroll.js
@@ -3,14 +3,15 @@ export class AppearOnScroll{
     this.offset = offset;
   }
   getAppearOnScrollElements(){
-    this.x = $(".appear-on-scroll").toArray();
+    this.x = document.getElementsByClassName("appear-on-scroll");
     return this.x;
   }
   AppearanceCheck(){
-    this.x.forEach(element=>{
-      if($(document).scrollTop() >= element.offsetTop+this.offset){
-        element.classList.remove("appear-on-scroll");
+    for(let a = 0;a<this.x.length;a++){
+      let page_scroll = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+      if(page_scroll >= this.x[a].offsetTop+this.offset){
+        this.x[a].classList.remove("appear-on-scroll");
       }
-    });
+    }
   }
 }
