0051b1f8db8c821265d03ef8dc343a6d96cd973f
ReedKrawiec <reedkrawiec@gmail.com>
2016-02-26 20:38:46 -0500

    Small Updates

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,10 @@ var gulp = require('gulp'),
     connect = require('gulp-connect'),
     sass = require('gulp-sass'),
     cssnano = require('gulp-cssnano'),
+    autoprefixer = require('gulp-autoprefixer'),
     uglify = require('gulp-uglify'),
+    imagemin = require('gulp-imagemin'),
+    pngquant = require('imagemin-pngquant'),
     rename = require("gulp-rename");
 
 gulp.task('babel', function () {
@@ -18,17 +21,6 @@ gulp.task('babel', function () {
       .pipe(rename("bundled.min.js"))
       .pipe(gulp.dest('script'));
 });
-gulp.task('connect', function() {
-    connect.server({
-        livereload: true,
-        port: 8888
-    });
-});
-
-gulp.task('hello', function() {
-  console.log('Hello Zell');
-});
-
 gulp.task('sass', function(){
   return gulp.src('sass/style.scss')
     .pipe(sass()) // Converts Sass to CSS with gulp-sass
@@ -36,8 +28,17 @@ gulp.task('sass', function(){
         browsers: ['last 2 versions'],
         cascade: false
     }))
-    //.pipe(cssnano())
+    .pipe(cssnano())
     .pipe(rename("style.min.css"))
     .pipe(gulp.dest('css'));
+});
+gulp.task('image_optimization', () => {
+	return gulp.src('images_dev/*')
+		.pipe(imagemin({
+			progressive: true,
+			svgoPlugins: [{removeViewBox: false}],
+			use: [pngquant()]
+		}))
+		.pipe(gulp.dest('images'));
 });
 gulp.task('default',['babel','sass']);
